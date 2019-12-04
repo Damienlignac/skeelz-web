4f03c28973ec07dd530c2bda61682631380e8f87
Damien <lignac.damien@gmail.com>
2019-12-04 10:00:16 +0100

    titre changé

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'formation-angular'`, () => {
+  it(`should have as title 'skeelz-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('formation-angular');
+    expect(app.title).toEqual('skeelz-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('formation-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('skeelz-web app is running!');
   });
 });
